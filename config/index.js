5b1b724fa14baf30b0306856c9f96d1e497533d8
wanfangqing <wanfangqing@huokefeng.cn>
2021-05-25 17:45:49 +0800

    test build

--- config/index.js
+++ config/index.js
@@ -1,31 +1,55 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.resolve(__dirname, "..", dir);
+}
 const config = {
-  projectName: 'wzj-marketing-mp',
-  date: '2021-5-25',
+  projectName: "wzj-marketing-mp",
+  date: "2021-5-25",
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2
   },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
+  sourceRoot: "src",
+  outputRoot: "dist",
   plugins: [],
   defineConstants: {
+    A: '"a"' // JSON.stringify('a')
+  },
+  sass: {
+    resource: ["src/styles/variable.scss", "src/styles/mixins.scss"],
+    projectDirectory: path.resolve(__dirname, "..")
+  },
+  alias: {
+    "@/components": resolve("src/components"),
+    "@/utils": resolve("src/utils"),
+    "@/package": resolve("package.json"),
+    "@/project": resolve("project.config.json")
   },
   copy: {
     patterns: [
+      // { from: "src/asset/tt/", to: "dist/asset/tt/", ignore: ["*.js"] }, // 指定需要 copy 的目录
+      // { from: "src/asset/tt/sd.jpg", to: "dist/asset/tt/sd.jpg" } // 指定需要 copy 的文件
     ],
     options: {
+      // ignore: ["*.js", "*.css"] // 全局的 ignore
     }
   },
-  framework: 'vue',
+  framework: "vue",
   mini: {
     postcss: {
-      pxtransform: {
+      // 可以进行 autoprefixer 的配置。配置项参考官方文档 https://github.com/postcss/autoprefixer
+      autoprefixer: {
         enable: true,
         config: {
-
+          // autoprefixer 配置项
         }
+      },
+      pxtransform: {
+        enable: true,
+        config: {}
       },
       url: {
         enable: true,
@@ -36,35 +60,34 @@ const config = {
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]"
         }
       }
     }
   },
   h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
+    publicPath: "/",
+    staticDirectory: "static",
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {
-        }
+        config: {}
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]"
         }
       }
     }
   }
-}
+};
 
-module.exports = function (merge) {
-  if (process.env.NODE_ENV === 'development') {
-    return merge({}, config, require('./dev'))
+module.exports = function(merge) {
+  if (process.env.NODE_ENV === "development") {
+    return merge({}, config, require("./dev"));
   }
-  return merge({}, config, require('./prod'))
-}
+  return merge({}, config, require("./prod"));
+};
